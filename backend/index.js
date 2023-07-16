a22160275be24fac6b1d73ee2a94c97891bbc753
Apx-Monstr <apxmonstr@gmail.com>
2023-07-17 02:31:20 +0530

    Backend API Implementation

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
-const app = require("express")();
-const PORT = 8080;
+const express = require('express');
+const axios = require("axios");
 
-app.post("/getWeather",(req, res)=>{
-    res.status(201).send(
-        {
-            hello:"from sample express server",
+const app = express();
+app.use(express.json());
+const PORT = 8080;
+const KEY = "f1f0698ed56d5c77a1fe1b2dbeaded58"
+
+app.get("/getWeather", (req,res)=>{
+    const reqData = req.body;
+    // console.log(reqData.cities);
+    cities = reqData.cities;
+    const data = {"weather":{}};
+    if (cities.length > 0){
+        for (const i in cities) {
+            const city = cities[i];
+            console.log(city);
+            axios.get('http://api.openweathermap.org/data/2.5/weather', {
+                params: {
+                  q: city,
+                  appid: KEY,
+                  units: 'metric',
+                }})
+            .then((response) => {
+                const resData = response.data;
+                data.weather[city] = Math.round(resData.main.temp) + "C";
+                if (Object.keys(data.weather).length === cities.length){
+                    res.status(201).send(data);
+                }
+            }).catch((err) => {
+                res.status(500).send(data);
+            });
         }
-    )
+    }else{
+        res.status(400).send(data);
+    }
 })
+
 app.listen(
     PORT,
     ()=>console.log("I am running at localhost:8080")
